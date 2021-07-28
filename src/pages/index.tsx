ade646fa5825e3615665b166c3ad64afcdaffed7
Tom Dong <contact@tomdong.io>
2021-07-27 19:07:30 -0700

    Updated store hours

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,9 +35,9 @@ const IndexPage = (): JSX.Element => {
   ]
 
   const hoursInfo = [
-    'Mon - Thur | 10:30 - 10:00',
-    'Fri - Sat | 10:30 - 11:00',
-    'Sunday | 11:30 - 10:00',
+    'Mon - Thur | 11:00 - 9:30',
+    'Fri - Sat | 11:00 - 10:30',
+    'Sunday | 11:30 - 9:30',
   ]
 
   return (
