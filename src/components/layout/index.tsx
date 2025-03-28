02a61535d6f09471ff597665b2db11ffac68c050
Tommy Dong <contact@tomdong.io>
2025-03-27 19:46:04 -0700

    Add hiring banner

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -16,6 +16,13 @@ const StyledPage: AnyStyledComponent = styled.div`
 
 const StyledMain: AnyStyledComponent = styled.main``
 
+const StyledAlert: AnyStyledComponent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: baseline;
+  text-align: center;
+`
+
 interface LayoutProps {
   children: React.ReactNode
 }
@@ -24,6 +31,15 @@ const Layout: React.FC<LayoutProps> = ({ children }): JSX.Element => {
   return (
     <>
       <StyledPage>
+        <StyledAlert>
+          <p>
+            We're hiring! Please email us at{' '}
+            <a href="mailto:sushiaimanchester@gmail.com">
+              sushiaimanchester@gmail.com
+            </a>{' '}
+            for more information
+          </p>
+        </StyledAlert>
         <Navbar />
         <StyledMain>{children}</StyledMain>
         <Footer />
